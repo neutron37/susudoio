219e4c10cdd1d357a885cac8577161ec624620f6
Neutron37 <neutron37@hauskreativ.com>
2018-04-20 02:25:35 +0200

    Start consolidating into fewer files.

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 whoami
 susudoio::runas_admin whoami
-susudoio::runas_admin ls -AlFh
+susudoio::runas_admin 'ls -AlFh ~/Documents'
 susudoio::runas_target whoami
-susudoio::runas_target ls -AlFh
+susudoio::runas_target 'ls  -AlFh ~/Documents'
 exit 37
